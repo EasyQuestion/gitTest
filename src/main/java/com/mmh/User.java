247e885602a6beff7ced2e0cb4f889b40bb0c177
mumh <minghui.mu@dadaabc.net>
2020-06-05 14:27:59 +0800

    添加用户

---
+++ src/main/java/com/mmh/User.java
@@ -0,0 +1,26 @@
+package com.mmh;
+
+/**
+ * @author mumh
+ * @date create in 2020/6/5 14:26
+ */
+public class User {
+    private Integer userId;
+    private String userName;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
